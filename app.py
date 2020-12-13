4d52af4314e823621653936d40115df47d9dc908
VET <73408920+VirtualEntertainmentTV@users.noreply.github.com>
2020-12-13 02:46:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home_page():
 @app.route('/<name>')
 def profile(name):
 	new_name = str(name) + "Welcome 2 The Future"
-	return render_template('index.html', name=new_name)
+	return render_template('index.html',  name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
